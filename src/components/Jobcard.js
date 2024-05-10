f8552c815b1d1140a7901c584a7e75414338fbd8
Rakesh Baddipadaga <rakeshbaddipadaga8@gmail.com>
2024-05-10 11:00:33 +0530

    init

---
+++ src/components/Jobcard.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Jobfilter from "./Jobfilter";
+
+function Jobcard(){
+
+    return(
+        <div>
+            <Jobfilter />
+            <h1>Card component</h1>
+        </div>
+    )
+}
+
+export default Jobcard;
---
+++ src/components/Jobfilter.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Jobfilter(){
+    return(
+        <div>
+            
+        </div>
+    )
+}
+
+export  default Jobfilter;
